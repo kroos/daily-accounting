2e62f0e6763e19c897aae558d2025fbd9af9219d
Kroos <dhiauddin@gmail.com>
2025-02-15 16:25:48 +0800

    -- fix nav bar dropdown

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,10 +1,10 @@
 <!-- navigator -->
-<nav class="navbar navbar-expand-lg align-self-start bg-primary rounded m-0 mb-1" data-theme="dark" style="--bg-opacity: .25;">
+<nav class="navbar navbar-expand-lg align-self-start bg-primary rounded m-0 mb-1" data-bs-theme="dark" style="--bg-opacity: .25;">
 	<div class="container">
 			<a class="navbar-brand" href="@auth{{ url('/dashboard') }}@else{{ url('/') }}@endauth">
 				{!! config('app.name') !!}<img src="">
 			</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarColor01">
