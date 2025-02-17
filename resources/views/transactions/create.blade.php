1a3b33571c8f8d910a5a5773392a0f2770c809c2
Noor Dhiauddin <dhiauddin@gmail.com>
2025-02-17 21:18:25 +0800

    -- integrate with HTML5-qrcode

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -88,22 +88,6 @@
 				</div>
 
 				<div class="col-sm-4 mx-auto my-2">
-					<!-- <button type="button" id="start-scanning" class="btn btn-sm btn-primary">Start scanning</button> -->
-					<button type="button" class="btn btn-sm btn-primary" id="startButton">Start</button>
-					<button type="button" class="btn btn-sm btn-primary" id="resetButton">Reset</button>
-					<!-- <video id="scannerCam" width="100%" height="300"></video> -->
-					<video id="video" width="100%" height="200" style="border: 1px solid gray"></video>
-					<!-- <pre id="result"></pre> -->
-
-					<div id="sourceSelectPanel" style="display:none">
-						<label for="sourceSelect">Change video source:</label>
-						<select id="sourceSelect" style="max-width:400px">
-						</select>
-					</div>
-
-					<label>Result:</label>
-					<pre><code id="result"></code></pre>
-
 				</div>
 
 				<div class="mt-3">
@@ -133,13 +117,8 @@
 	</div>
 
 </div>
-<!-- <script src=" https://cdn.jsdelivr.net/npm/quagga@0.12.1/dist/quagga.min.js "></script> -->
-<!-- <script src="https://unpkg.com/@zxing/browser"></script> -->
-<script type="text/javascript" src="https://unpkg.com/@zxing/library@latest/umd/index.min.js"></script>
-<!-- <script src="https://unpkg.com/@zxing/library"></script> -->
-<!-- <script src="https://unpkg.com/@zxing/browser@latest"></script> -->
-<!-- <script src="https://unpkg.com/@zxing/library@latest"></script> -->
-
+<script src=" https://cdn.jsdelivr.net/npm/quagga@0.12.1/dist/quagga.min.js "></script>
+<script src="https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js"></script>
 @endsection
 
 @section('js')
@@ -178,144 +157,55 @@ $("#date").jqueryuiDatepicker({
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-window.addEventListener('load', function () {
-	let selectedDeviceId;
-	const codeReader = new ZXing.BrowserMultiFormatReader()
-	console.log('ZXing code reader initialized')
-	codeReader.listVideoInputDevices()
-	.then((videoInputDevices) => {
-		const sourceSelect = document.getElementById('sourceSelect')
-		selectedDeviceId = videoInputDevices[0].deviceId
-		if (videoInputDevices.length >= 1) {
-			videoInputDevices.forEach((element) => {
-				const sourceOption = document.createElement('option')
-				sourceOption.text = element.label
-				sourceOption.value = element.deviceId
-				sourceSelect.appendChild(sourceOption)
-			})
+////////////////////////////////////////////////////////////////////////////////////////////
+let scanner = new Html5Qrcode("scannerCamera");
+let isScanning = false;
 
-			sourceSelect.onchange = () => {
-				selectedDeviceId = sourceSelect.value;
-			};
-
-			const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-			sourceSelectPanel.style.display = 'block'
+// Function to start scanning
+function startScanner() {
+	if (!isScanning) {
+		scanner.start(
+		{ facingMode: "environment" }, // Use the back camera
+		{
+			fps: 10,    // Frames per second
+			qrbox: 250  // Size of the scanning box
+		},
+		(decodedText) => {
+			$("#barcode").val(decodedText); // Set barcode value
+			$("#scanbarcde").modal("hide"); // Close modal
+			stopScanner(); // Stop scanning
+		},
+		(errorMessage) => {
+			console.warn("Scanning error:", errorMessage);
 		}
+		).then(() => {
+			isScanning = true;
+		}).catch((err) => {
+			console.error("Scanner initialization failed:", err);
+		});
+	}
+}
 
-		document.getElementById('startButton').addEventListener('click', () => {
-			codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-				if (result) {
-					console.log(result)
-					document.getElementById('result').textContent = result.text
-				}
-				if (err && !(err instanceof ZXing.NotFoundException)) {
-					console.error(err)
-					document.getElementById('result').textContent = err
-				}
-			})
-			console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
-		})
+// Function to stop scanner
+function stopScanner() {
+	if (isScanning) {
+		scanner.stop().then(() => {
+			isScanning = false;
+		}).catch((err) => {
+			console.error("Scanner stop error:", err);
+		});
+	}
+}
 
-		document.getElementById('resetButton').addEventListener('click', () => {
-			codeReader.reset()
-			document.getElementById('result').textContent = '';
-			console.log('Reset.')
-		})
+// Start scanner when modal opens
+$("#scanbarcde").on("shown.bs.modal", function () {
+	startScanner();
+});
 
-	})
-	.catch((err) => {
-		console.error(err)
-	})
-})
-
-////////////////////////////////////////////////////////////////////////////////////////////
-// document.addEventListener("DOMContentLoaded", function () {
-// 	let codeReader = new ZXing.BrowserMultiFormatReader();
-// 	let selectedDeviceId = null;
-//
-// 	// Get elements
-// 	// const scanBarcodeBtn = document.getElementById("scanBarcode");
-// 	const scanBarcodeBtn = document.getElementById("start-scanning");
-// 	const barcodeInput = document.getElementById("barcode");
-// 	// const scannerCamera = document.getElementById("scannerCamera");
-// 	const scannerCamera = document.getElementById("scannerCam");
-// 	const modalElement = document.getElementById("scanbarcde");
-//
-// 	// Start scanning when clicking "Scan Barcode"
-// 	scanBarcodeBtn.addEventListener("click", function () {
-// 		codeReader.getVideoInputDevices().then((videoInputDevices) => {
-// 			if (videoInputDevices.length > 0) {
-// 				selectedDeviceId = videoInputDevices[0].deviceId;
-// 				startScanner();
-// 			} else {
-// 				alert("No camera found!");
-// 			}
-// 		}).catch((err) => {
-// 			console.error("Camera error:", err);
-// 		});
-// 	});
-//
-// 	function startScanner() {
-// 		codeReader.decodeFromVideoDevice(selectedDeviceId, scannerCamera, (result, err) => {
-// 			if (result) {
-// 				barcodeInput.value = result.text; // Fill barcode input
-// 				console.log("Scanned Barcode:", result.text);
-// 				closeModal(); // Close modal
-// 			}
-// 		});
-// 	}
-//
-// 	// Stop scanning when modal closes
-// 	modalElement.addEventListener("hidden.bs.modal", function () {
-// 		codeReader.reset();
-// 	});
-//
-// 	function closeModal() {
-// 		let modal = document.querySelector(".modal.show");
-// 		if (modal) {
-// 			let closeBtn = modal.querySelector("[data-bs-dismiss='modal']");
-// 			closeBtn.click();
-// 		}
-// 	}
-// });
-
-////////////////////////////////////////////////////////////////////////////////////////////
-// 	$(document).ready(function () {
-// 		let codeReader = new ZXing.BrowserMultiFormatReader();
-// 		let selectedDeviceId = null;
-//
-// 		// When modal opens, start scanning
-// 		$('#scanbarcde').on('shown.bs.modal', function () {
-// 			codeReader.getVideoInputDevices().then((videoInputDevices) => {
-// 				if (videoInputDevices.length > 0) {
-// 					selectedDeviceId = videoInputDevices[0].deviceId;
-// 					startScanner();
-// 				} else {
-// 					alert("No camera found!");
-// 				}
-// 			}).catch((err) => {
-// 				console.error("Camera error:", err);
-// 			});
-// 		});
-//
-// 	function startScanner() {
-// 		codeReader.decodeFromVideoDevice(selectedDeviceId, 'scannerCamera', (result, err) => {
-// 			if (result) {
-// 				$("#barcode").val(result.text); // Fill barcode input
-// 				console.log("Scanned Barcode:", result.text);
-// 				$('#scanbarcde').modal('hide'); // Close modal
-// 				codeReader.reset(); // Stop scanner
-// 			}
-// 		}).catch((err) => {
-// 			console.error("Scanner error:", err);
-// 		});
-// 	}
-//
-// 	// Stop scanning when modal closes
-// 	$('#scanbarcde').on('hidden.bs.modal', function () {
-// 		codeReader.reset();
-// 	});
-//  });
+// Stop scanner when modal closes
+$("#scanbarcde").on("hidden.bs.modal", function () {
+	stopScanner();
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // $('#scanBarcode').click(function () {
@@ -324,7 +214,6 @@ window.addEventListener('load', function () {
 // 		Quagga.stop();
 // 		$('#scannerModal').modal('hide');
 // 	});
-//
 // 	Quagga.init({
 // 		inputStream: {
 // 			type: "LiveStream",
@@ -344,7 +233,6 @@ window.addEventListener('load', function () {
 // 		console.log("Quagga initialized successfully");
 // 		Quagga.start();
 // 	});
-//
 // 	Quagga.onDetected(function (data) {
 // 		$('#barcode').val(data.codeResult.code);
 // 		Quagga.stop();
