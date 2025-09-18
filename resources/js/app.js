16ce9954ef9a1c174718d162c7c263d75d3be819
Dhiauddin <dhiauddin@gmail.com>
2025-09-19 01:03:23 +0800

    -- integrate addremoverow jquery plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 // Default Laravel bootstrapper, installs axios, jQuery
-import './bootstrap';
+import './jquery';
 
 // jquery-ui
 import './jquery-ui-prefix';
@@ -49,21 +49,10 @@ window.DataTable = DataTable;
 import '@claviska/jquery-minicolors';
 
 // Chart.js
-import Chart from 'chart.js/auto';
-window.Chart = Chart;
+import  './chart';
 
-// @fullcalendar
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import multiMonthPlugin from "@fullcalendar/multimonth";
-import listPlugin from '@fullcalendar/list';
-import momentPlugin from '@fullcalendar/moment';
-import bootstrap5Plugin from '@fullcalendar/bootstrap5';
-window.Calendar = Calendar;
-window.multiMonthPlugin = multiMonthPlugin;
-window.dayGridPlugin = dayGridPlugin;
-window.timeGridPlugin = timeGridPlugin;
-window.listPlugin = listPlugin;
-window.momentPlugin = momentPlugin;
-window.bootstrap5Plugin = bootstrap5Plugin;
+// fullcalendar
+import './fullcalendar';
+
+// addRemoveRow
+import	'./addRemoveRowjQueryPlugins';
